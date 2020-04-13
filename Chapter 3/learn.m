67af338ed309651681788f9296bd13e400a229c3
howayShi <35300386+howayShi@users.noreply.github.com>
2020-04-13 15:04:39 +0800

    Update learn.m

--- Chapter 3/learn.m
+++ Chapter 3/learn.m
@@ -1,1 +1,14 @@
+%3.1%
+A=[-2 -2.5 -0.5;1 0 0;0 1 0];
+B=[1;0;0];
+C=[0 1.5 1];
+D=0;
+sys=ss(A,B,C,D);
+%initial(sys,[2 2 2]) 
+[y,t,x]=initial(sys,[2 2 2],60) %60为指定时间
+subplot(121)
+plot(t,x)
+subplot(122)
+plot(t,y)
 
+%3.2%
